950e50fb60d8faa04b0606817d48228c4d85eea6
aggarwal2000 <aggarwal2000chd@gmail.com>
2022-06-04 14:10:44 +0530

    Add batch parilu and isai-ilu public interface

--- include/ginkgo/ginkgo.hpp
+++ include/ginkgo/ginkgo.hpp
@@ -113,8 +113,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <ginkgo/core/preconditioner/batch_exact_ilu.hpp>
 #include <ginkgo/core/preconditioner/batch_ilu.hpp>
+#include <ginkgo/core/preconditioner/batch_ilu_isai.hpp>
 #include <ginkgo/core/preconditioner/batch_isai.hpp>
 #include <ginkgo/core/preconditioner/batch_jacobi.hpp>
+#include <ginkgo/core/preconditioner/batch_par_ilu.hpp>
 #include <ginkgo/core/preconditioner/ic.hpp>
 #include <ginkgo/core/preconditioner/ilu.hpp>
 #include <ginkgo/core/preconditioner/isai.hpp>
